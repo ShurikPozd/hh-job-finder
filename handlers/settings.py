d8b50e8025a7dab87a0680de28c71be8f2e6aecd
ShurikPozd <shurik-3002@mail.ru>
2026-09-17 02:34:05 +0300

    fix: импорт InlineKeyboardMarkup в настройках (NameError в cb_set)

--- handlers/settings.py
+++ handlers/settings.py
@@ -3,7 +3,7 @@ import logging
 
 from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
